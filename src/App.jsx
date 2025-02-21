58d407e7606d3725560bd7e7ae4ddb6e7418c697
tannaznamdar <tannaznamdar99@gmail.com>
2025-02-21 21:35:28 +0330

    create menu list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Logo from "./assets/components/Logo";
+import MenuList from "./assets/components/MenuList";
 
 export default function App() {
+
+  let menuList =[ 'Home' , 'About us' , 'Services' , 'Blog']
+
+
   return (
     <>
       <div className="container mx-auto">
         <div className="mt-7">
           <Logo />
+          <MenuList menuList={menuList} />
         </div>
       </div>
     </>
