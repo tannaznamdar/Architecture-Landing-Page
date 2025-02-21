f54c1708b8fb244feea14dae9ab3badb79e74579
tannaznamdar <tannaznamdar99@gmail.com>
2025-02-21 20:31:34 +0330

    Create logo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Logo from "./assets/components/Logo";
 
 export default function App() {
   return (
     <>
       <div className="container mx-auto">
+        <div className="mt-7">
+          <Logo />
+        </div>
       </div>
     </>
   );
---
+++ src/assets/components/Logo.jsx
@@ -0,0 +1,10 @@
+export default function Logo(){
+    return(
+        <div className="flex">
+            <span className="font-raleway text-3xl text-black font-bold"> Ima </span>
+            <div className="bg-black rounded-full  px-3 block">
+                <span className="font-raleway text-[26px] text-white font-extrabold"> h </span>
+            </div>
+        </div>
+    )
+}
