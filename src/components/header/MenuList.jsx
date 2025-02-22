938f5a09bf791ddd10af48871d1deb856303aacd
tannaznamdar <tannaznamdar99@gmail.com>
2025-02-22 10:36:46 +0330

    Add a dynamic active class to the menu

--- src/components/header/MenuList.jsx
+++ src/components/header/MenuList.jsx
@@ -1,9 +1,21 @@
+import { NavLink } from "react-router";
+
 export default function MenuList({ menuList }) {
   return (
     <nav>
       <ul className="flex font-inconsolata  text-xl uppercase  gap-[50px] ">
         {menuList.map((item) => (
-          <li className="hover:underline font-semibold hover:font-bold text-gray-800 hover:text-black underline-offset-5 decoration-2 decoration-gray-600 tracking-tight cursor-pointer" key={item.name}>{item.name}</li>
+          <NavLink
+            key={item.name}
+            to={item.href}
+            className={({ isActive, isPending }) =>
+              `${
+                isActive ? "underline font-bold text-black" : ""
+              } font-semibold  text-gray-800 underline-offset-5 decoration-2 decoration-gray-600 tracking-tight cursor-pointer`
+            }
+          >
+            <li>{item.name}</li>
+          </NavLink>
         ))}
       </ul>
     </nav>
