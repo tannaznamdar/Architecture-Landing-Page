381d1c2c3e21ed6e3e00bb97cb1d51af60859f67
tannaznamdar <tannaznamdar99@gmail.com>
2025-02-23 18:33:17 +0330

    Delete font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        raleway: ['Raleway', 'sans-serif'],       
-        inconsolata: ['Inconsolata', 'monospace'],
-      }
+
     },
   },
   plugins: [],
