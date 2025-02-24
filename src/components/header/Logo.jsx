4422c5445e6b166aa62089d9d2e619c9e966b042
tannaznamdar <tannaznamdar99@gmail.com>
2025-02-24 10:55:53 +0330

    Adding other font weights

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,9 +1,9 @@
 export default function Logo(){
     return(
-        <div className="flex">
-            <span className="font-raleway text-3xl text-black font-bold"> Ima </span>
+        <div className="flex gap-0.5">
+            <span className="font-raleway text-3xl text-black font-extrabold"> Ima </span>
             <div className="bg-black rounded-full  px-3 block">
-                <span className="font-raleway text-[26px] text-white font-extrabold"> h </span>
+                <span className="font-raleway text-[26px] text-white font-[900]"> h </span>
             </div>
         </div>
     )
