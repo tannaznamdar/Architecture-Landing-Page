aeace0a7fa4413fdbded721ff6c3dd1a49ba84f6
tannaznamdar <tannaznamdar99@gmail.com>
2025-02-24 13:02:20 +0330

    Add logo-swiper component to home page

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,10 +1,27 @@
 import image1 from "../assets/images/image-1.png";
+import logo1 from "../assets/images/Logo1.png";
+import logo2 from "../assets/images/Logo2.png";
+import logo3 from "../assets/images/Logo3.png";
+
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
+import LogoSwiper from "../components/swiper/Logo-swiper";
 
 export default function HomePage() {
+  const logoImage = [
+    logo1,
+    logo2,
+    logo3,
+    logo1,
+    logo2,
+    logo3,
+    logo1,
+    logo2,
+    logo3,
+  ];
+
   return (
     <>
-      <div className="w-[90%] ml-auto">
+      <div className="w-[90%] ml-auto mb-[90px]">
         <div className="flex items-center">
           <div className="w-1/2">
             <div className="w-3/4">
@@ -17,7 +34,9 @@ export default function HomePage() {
                 pharetra non. Integer lorem in scelerisque tortor dui tempor
               </p>
               <button className="bg-black px-6 py-2.5 flex gap-3.5 items-center uppercase cursor-pointer">
-                <span className="text-white text-lg font-semibold font-inconsolata leading-0"> VIEW A PROJECT </span>
+                <span className="text-white text-lg font-semibold font-inconsolata leading-0">
+                  VIEW A PROJECT
+                </span>
                 <ArrowRightIcon className="size-5 text-white" />
               </button>
             </div>
@@ -32,6 +51,19 @@ export default function HomePage() {
               />
             </figure>
           </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto flex mb-[90px]">
+        <div className="w-1/4">
+          <div className="w-1/2">
+            <span className="text-black font-raleway font-bold text-2xl ">
+              Trusted by 75+ Partners
+            </span>
+          </div>
+        </div>
+        <div className="w-3/4">
+          <LogoSwiper logoImage={logoImage} />
         </div>
       </div>
     </>
