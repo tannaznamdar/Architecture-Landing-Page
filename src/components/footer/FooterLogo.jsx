1e834dfce5e5af86698643be4e17d2916cf50e41
tannaznamdar <tannaznamdar99@gmail.com>
2025-02-27 10:34:33 +0330

    Create footer logo component

---
+++ src/components/footer/FooterLogo.jsx
@@ -0,0 +1,10 @@
+export default function FooterLogo(){
+    return(
+        <div className="flex gap-0.5 items-center">
+            <span className="font-raleway text-5xl text-gray-700 font-extrabold"> Ima </span>
+            <div className="bg-gray-700 rounded-full items-center py-1 px-3.5 block">
+                <span className="font-raleway text-4xl text-white font-[900]"> h </span>
+            </div>
+        </div>
+    )
+}
