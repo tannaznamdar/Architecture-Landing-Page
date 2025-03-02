5d92eb02b2596e5d2e15ab4d33a3b96823b70df4
tannaznamdar <tannaznamdar99@gmail.com>
2025-03-02 21:47:15 +0330

    Scroll to top when reload page

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function Root() {
   return (
     <div>
+      <ScrollToTop />
       <Header />
-      
+
       <div className="min-h-screen">
         <Outlet />
       </div>
