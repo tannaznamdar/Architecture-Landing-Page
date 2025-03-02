6de85cf958586e3d9148fe0de627fc06a7a89537
tannaznamdar <tannaznamdar99@gmail.com>
2025-03-02 21:33:54 +0330

    Create contact us page

---
+++ src/views/ContactUsPage.jsx
@@ -0,0 +1,11 @@
+export default function ContactUsPage() {
+
+    return (
+      <>
+        <div className="container mx-auto">
+           <h1> Contact Us Page </h1>
+        </div>
+      </>
+    );
+  }
+  
