3d88518ed0c304c3af4e1ba58317e2518c844cd4
tannaznamdar <tannaznamdar99@gmail.com>
2025-03-03 11:22:24 +0330

    Responsive - footer logo

--- src/components/footer/FooterLogo.jsx
+++ src/components/footer/FooterLogo.jsx
@@ -1,10 +1,14 @@
-export default function FooterLogo(){
-    return(
-        <div className="flex gap-0.5 items-center">
-            <span className="font-raleway text-5xl text-gray-700 font-extrabold"> Ima </span>
-            <div className="bg-gray-700 rounded-full items-center py-1 px-3.5 block">
-                <span className="font-raleway text-4xl text-white font-[900]"> h </span>
-            </div>
-        </div>
-    )
+export default function FooterLogo() {
+  return (
+    <div className="flex gap-0.5 items-center">
+      <span className="font-raleway lg:text-5xl text-4xl text-gray-700 font-extrabold">
+        Ima
+      </span>
+      <div className="bg-black opacity-70 rounded-full items-center py-1 lg:px-3.5 px-3 block">
+        <span className="font-raleway lg:text-4xl text-3xl text-white font-[900]">
+          h
+        </span>
+      </div>
+    </div>
+  );
 }
