2a466581a39210a85ca935f9eda507e95f586061
tannaznamdar <tannaznamdar99@gmail.com>
2025-03-05 12:21:26 +0330

    Create Image swiper

---
+++ src/components/swiper/ImageSwiper.jsx
@@ -0,0 +1,48 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import { useRef } from "react";
+import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/solid";
+
+export default function ImageSwiper({ projectImg }) {
+  const swiperRef = useRef(null);
+
+  return (
+    <div className="relative">
+      <Swiper
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 7000, disableOnInteraction: false }}
+        modules={[Navigation, Autoplay]}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
+      >
+        {projectImg.map((item, index) => (
+          <SwiperSlide key={index}>
+            <figure className="h-[500px]">
+              <img src={item} alt="#" className="w-full h-full object-cover" />
+            </figure>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <div className=" backdrop-blur-xs px-[15px] py-2 absolute bottom-0 right-0 z-10">
+        <div className="flex gap-5">
+          <button
+            onClick={() => swiperRef.current?.slideNext()}
+            className=" text-white transition"
+          >
+            <ChevronLeftIcon className="hover:text-gray-800 size-5" />
+          </button>
+
+          <button
+            onClick={() => swiperRef.current?.slidePrev()}
+            className=" text-white transition"
+          >
+            <ChevronRightIcon className="hover:text-gray-800 size-5" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
